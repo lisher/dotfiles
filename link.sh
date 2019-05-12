99c8541788a096e04039bddf1fd29a21d0cdb95a
lisher <lisher.bass@gmail.com>
2019-05-12 13:33:49 +0200

    Linking script updated to create backup files

--- link.sh
+++ link.sh
@@ -11,18 +11,35 @@ GITIGNORE=~/.gitignore
 
 TMUX_CONF=~/.tmux.conf
 
+BACKUP_EXT=.bac
+
+# 0 - success
+# 1 - not all files were linked
+status=0
+
 # $1 - target config file
 # $2 - relative path to file in this repo to link as target
 create_link()
 {
   if [ ! -f $CURRENT_REPO/$2 ]; then
     echo "File $2 doesn't exists in this repository, path $CURRENT_REPO/$2"
-    return 1
+    exit 2
   fi
 
   if [ -L $1 ]; then
-    echo "$1 file exists and is a symbolic link, unlinking"
+    echo "$1 file exists and is a symbolic link (to `readlink -f $1`), unlinking"
     unlink $1
+  fi
+
+  if [ -f $1 ]; then
+    if [ -f $1$BACKUP_EXT ]; then
+      echo "File $1 exists and backup file $1$BACKUP_EXT is present, linking skipped"
+      status=1
+      return
+    else
+      echo "File $1 exists, creating backup $1$BACKUP_EXT"
+      mv $1 $1$BACKUP_EXT
+    fi
   fi
 
   ln -s $CURRENT_REPO/$2 $1
@@ -36,3 +53,7 @@ create_link $GITCONFIG git/gitconfig
 create_link $GITIGNORE git/gitignore
 
 create_link $TMUX_CONF tmux/tmux_conf
+
+if [ $status -eq 1 ]; then
+  echo "\n Not all files were correctly linked!"
+fi
